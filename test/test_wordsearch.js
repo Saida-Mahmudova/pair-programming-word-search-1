7dd63903a74be351f7ba28ca8676a747cfbde01b
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-17 04:26:07 +0300

    Pair programming with Nicole Maclean

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,24 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+  it("should return true if the word is present vertically", function () {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['F', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  it("should return false if array is empty", function () {
+    const result = wordSearch([], 'SEINFELD')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//pair programming - Nicole MacLean && Saida Mahmudova
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return false;
+    }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    const verticalJoin = matrix(letters).map(ls => ls.join(''));
+
+    for (let l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+        return false;
+    }
+};
+
+const matrix = function (letters) {
+    let newArray = [];
+    for (let r = 0; r < letters.length; r++) {
+        for (let c = 0; c < letters[r].length; c++) {
+            if (!newArray[c]) {
+                newArray.push([letters[r][c]]);
+            } else {
+                newArray[c].push(letters[r][c]);
+            }
+        }
+    }
+    return newArray;
+};
+module.exports = wordSearch;
+
+
